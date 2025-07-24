674a1b61dcdfe9a8195f1c5f42cda1ed0d2cb115
Ziad-Elganzory <ziadmohamed770@outlook.com>
2025-07-24 18:08:00 +0300

    Dependency Injection Container

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use App\App;
 use App\Config;
+use App\Container;
 use App\Controllers\HomeController;
 use App\Controllers\UserController;
 use App\Router;
@@ -16,7 +17,9 @@ $dotenv->load();
 define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEWS_PATH', __DIR__ . '/../views');
 
-$router = new Router();
+
+$container = new Container();
+$router = new Router($container);
 
 $router
     ->get('/',[HomeController::class, 'index'])
